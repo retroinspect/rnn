127794849b39ce2936df3013e142ae31b28d7541
retro <sangachoi2103@gmail.com>
2021-07-12 17:18:39 +0000

    add update bounds to ignore padding

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ n_train_data = int(len_data * 0.9)
 n_val_data = len_data - n_train_data
 train_dataset, val_dataset = random_split(mydataset, [n_train_data, n_val_data])
 
-batch_size = 32
+batch_size = 8
 
 def labelToTensor(label):
   return torch.tensor([mydataset.all_categories.index(label)], dtype=torch.long)
@@ -93,7 +93,6 @@ def labelToTensor(label):
 def collate_batch(batch):
   # print(batch) # {"name": ["sanga", "sangmin"], "label": ["Korea", "Korea"]}
 
-  # TODO employ pack_padded_sequence
   names = [ batch[i]["name"] for i in range(len(batch)) ]
   name_lengths = torch.LongTensor([len(name) for name in names])
   max_len = max(name_lengths)
@@ -105,9 +104,13 @@ def collate_batch(batch):
   names = names[:, sorted_idx]
   labels = labels[sorted_idx]
 
-  packed_names = pack_padded_sequence(names, name_lengths.tolist())
+  # TODO update_bounds 좀 더 효율적으로 짜기
+  update_bounds = torch.LongTensor([0] * max_len) # 한 번에 process하는 글자 개수 
+  for name_len in name_lengths:
+    tmp = torch.LongTensor([1] * name_len + [0] * (max_len - name_len))
+    update_bounds += tmp;
 
-  return (names, labels)
+  return (names, labels, update_bounds)
 
 
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_batch)
@@ -141,11 +144,19 @@ class RNN(nn.Module):
         self.i2o = nn.Linear(input_size + hidden_size, output_size)
         self.softmax = nn.LogSoftmax(dim=1)
 
-    def forward(self, input, hidden):
+    def forward(self, input, hidden, update_bound=None):
+        old_hidden = hidden
         combined = torch.cat((input, hidden), dim=1)
         hidden = self.i2h(combined)
         output = self.i2o(combined)
         output = self.softmax(output)
+
+        # update_bound 미만의 index에 대해서만 hidden update
+        # 이상의 index에 대해서는 old_hidden 사용
+        if (update_bound != None):
+          hidden = torch.cat([hidden[0:update_bound, :], old_hidden[update_bound:, :]])
+        # update_bound = 4 이면 hidden[3, :] + old_hidden[4:, :] 
+
         return output, hidden
 
     def initHidden(self, sample_batched_size):
@@ -170,7 +181,7 @@ learning_rate = 0.005 # for SGD
 import time
 import math
 
-n_iters = 100
+n_iters = 20
 print_every = 1
 plot_every = 1000
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
@@ -197,17 +208,20 @@ for iter in range(n_iters):
   # for each mini-batch
   for batch_idx, sample_batched in enumerate(train_loader):
 
-    name_tensor, label_tensor = sample_batched
+    name_tensor, label_tensor, update_bounds = sample_batched
     hidden = model.initHidden(name_tensor.size(1))
 
     if useGPU:
       label_tensor = label_tensor.cuda()
       name_tensor = name_tensor.cuda()
+      update_bounds = update_bounds.cuda()
       hidden = hidden.cuda()
 
-    #feed into model letter by letter
+    # feed into model letter by letter
     for i in range(name_tensor.size(0)):
-      pred, hidden = model(name_tensor[i], hidden)
+      pred, hidden = model(name_tensor[i], hidden, update_bounds[i])
+      # pred, hidden = model(name_tensor[i], hidden, None) # 비교용
+
 
     optimizer.zero_grad()
     model.zero_grad()
@@ -227,15 +241,17 @@ for iter in range(n_iters):
   total_loss, total_cnt, correct_cnt = 0.0, 0.0, 0.0
   for batch_idx, sample_batched in enumerate(valid_loader):
     with torch.no_grad():
-      name_tensor, label_tensor = sample_batched  
+      name_tensor, label_tensor, update_bounds = sample_batched  
       hidden = model.initHidden(name_tensor.size(1))
       if useGPU:
         label_tensor = label_tensor.cuda()
         name_tensor = name_tensor.cuda()
+        update_bounds = update_bounds.cuda()
         hidden = hidden.cuda()
 
       for i in range(name_tensor.size(0)):
-        pred, hidden = model(name_tensor[i], hidden)
+        pred, hidden = model(name_tensor[i], hidden, update_bounds[i])
+        # pred, hidden = model(name_tensor[i], hidden, None) # 비교용
 
     loss = criterion(pred, label_tensor)
     total_loss += loss.item() # accumulate loss
